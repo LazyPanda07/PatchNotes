f0cdfbfc26c5260b66ae1eba6a5008bc6aab7629
LazyPanda07 <semengricenko@gmail.com>
2021-07-27 03:12:03 +0500

    Added simple window

--- PatchNotes/src/EntryPoint.cpp
+++ PatchNotes/src/EntryPoint.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
 #include "GUIFramework.h"
+#include "Composites/SeparateWindow.h"
+#include "Utility/Holders/WindowHolder.h"
+
+#pragma warning(disable: 28251)
 
 using namespace std;
 
-int main(int argc, char** argv)
+CREATE_DEFAULT_WINDOW_FUNCTION(patchNotes)
+
+int wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
 	try
 	{
 		gui_framework::GUIFramework& instance = gui_framework::GUIFramework::get();
 
+		gui_framework::WindowHolder holder(make_unique<gui_framework::SeparateWindow>(L"PatchNotesWindow", L"Patch notes", gui_framework::utility::ComponentSettings(300, 200, 800, 600), "patchNotes"));
+		gui_framework::SeparateWindow* mainWindow = dynamic_cast<gui_framework::SeparateWindow*>(holder.get());
 
+		mainWindow->setExitMode(gui_framework::BaseComponent::exitMode::quit);
+
+		holder.runMainLoop();
 	}
 	catch (const exception& e)
 	{
