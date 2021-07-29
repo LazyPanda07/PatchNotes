661e057235d00440c26eff6e8f05bdf083d7b610
LazyPanda07 <semengricenko@gmail.com>
2021-07-30 01:21:24 +0500

    Added CategoryConfigurationController implementation

--- PatchNotes/src/Controllers/CategoryConfigurationController.cpp
+++ PatchNotes/src/Controllers/CategoryConfigurationController.cpp
@@ -2,6 +2,7 @@
 
 #include "Models/CategoryConfigurationModel.h"
 #include "PatchNotesUtility.h"
+#include "Validation.h"
 
 using namespace std;
 
@@ -9,9 +10,17 @@ namespace controllers
 {
 	json::JSONBuilder CategoryConfigurationController::collectData(gui_framework::BaseComposite* window) const
 	{
-		json::JSONBuilder builder(utility::getCodepage());
+		uint32_t codepage = utility::getCodepage();
+		json::JSONBuilder builder(codepage);
+		gui_framework::EditControl* categoryEditControl = dynamic_cast<gui_framework::EditControl*>(window->findChild(L"CategoryName"));
+		string projectFile = gui_framework::utility::to_string(dynamic_cast<gui_framework::StaticControl*>(window->findChild(L"ProjectCategory"))->getText(), codepage);
+		string categoryName = gui_framework::utility::to_string(categoryEditControl->getText(), codepage);
 
+		validation::emptyValidation(categoryName, categoryEditControl->getPlaceholder());
 
+		builder.
+			append("projectFile", move(projectFile)).
+			append("category", move(categoryName));
 
 		return builder;
 	}
