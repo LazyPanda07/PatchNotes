2fac808ee2c3533d504f998de1ae11063ad5977a
LazyPanda07 <semengricenko@gmail.com>
2021-08-30 12:38:32 +0500

    Refactor

--- PatchNotes/src/Models/DeleteProjectConfigurationModel.cpp
+++ PatchNotes/src/Models/DeleteProjectConfigurationModel.cpp
@@ -9,11 +9,20 @@ namespace models
 	json::JSONBuilder DeleteProjectConfigurationModel::processData(const json::JSONParser& data)
 	{
 		filesystem::path projectToDelete = data.getString("projectToDelete");
+		string message;
+		bool success = filesystem::remove(projectToDelete);
 
-		filesystem::remove(projectToDelete);
+		if (success)
+		{
+			message = format("Конфигурация {} была успешно удалена", projectToDelete.filename().stem().string());
+		}
+		else
+		{
+			message = format("Не удалось удалить {}", projectToDelete.filename().stem().string());
+		}
 
 		return json::JSONBuilder(1251).
-			append("success", true).
-			append("message", format("Конфигурация {} была успешно удалена", projectToDelete.filename().stem().string()));
+			append("success", success).
+			append("message", move(message));
 	}
 }
